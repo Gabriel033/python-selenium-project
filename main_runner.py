13727c10a8913929e23cc819c368759a134c53fb
Jose Gabriel Castillo Garcia <gabriel.castillo@ibm.com>
2024-01-24 14:52:28 -0600

    Se agrega otra manera para ejecutar

--- main_runner.py
+++ main_runner.py
@@ -4,3 +4,4 @@ tags = "Escenario_01"
 
 if __name__ == '__main__':
     main("-f allure_behave.formatter:AllureFormatter -o reports/ -f pretty --no-capture --no-color -k --tags "+str(tags)+"")
+    # main("behave -n 'Buying a product in Amazon2'")
